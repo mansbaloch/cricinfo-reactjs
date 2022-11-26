ddea995a5ace4104b3e69f8bd5be7c1b1c192aa5
mansbaloch <114749425+mansbaloch@users.noreply.github.com>
2022-11-26 15:58:09 +0500

    Create RightSideBar.jsx

---
+++ src/Component/Pages/RightSideBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function RightSideBar() {
+  return (
+    <div><div class="card mb-3">
+  
+  <div class="card-body">
+    <h5 class="card-title">Quaid-e-Azam Trophy</h5>
+    <hr />
+    <p class="a card-text ">NOT COVERED LIVE</p>
+    <p class="card-text">Sindh</p>
+    <p class="card-text">Balochistan*</p>
+    <p class= "b card-text"><h6>(77 ov) <h4>349/1</h4></h6> </p>
+
+    
+  </div>
+</div></div>
+  )
+}
